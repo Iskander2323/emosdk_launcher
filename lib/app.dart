1b8619615ccb5911229d5de08c8b05e9c25e92ee
Iskander2323 <iska200314@gmail.com>
2025-09-17 19:13:58 +0500

    First commit Empty projecty

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:emosdk_launcher/components/main_page.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+ @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MainPage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:emosdk_launcher/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
