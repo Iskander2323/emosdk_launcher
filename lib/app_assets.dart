53e5356f13be6387de39f943f46501dc1838c59f
Iskander2323 <iska200314@gmail.com>
2025-10-01 22:33:28 +0500

    Add game assets and asset references
    Added image and video assets for multiple game teams and updated pubspec.yaml to include asset directories. Introduced AppAssets class to provide static references to these assets for use in the application.

---
+++ lib/app_assets.dart
@@ -0,0 +1,93 @@
+class AppAssets {
+  static const String echo_games_lab_video =
+      'assets/echo_games_lab/videos/teaser.mp4';
+  static const String echo_games_lab_src1 =
+      'assets/echo_games_lab/images/src_1.png';
+  static const String echo_games_lab_src2 =
+      'assets/echo_games_lab/images/src_2.png';
+  static const String echo_games_lab_src3 =
+      'assets/echo_games_lab/images/src_3.png';
+  static const String echo_games_lab_src4 =
+      'assets/echo_games_lab/images/src_4.png';
+  static const String echo_games_lab_src5 =
+      'assets/echo_games_lab/images/src_5.png';
+  static const String echo_games_lab_src6 =
+      'assets/echo_games_lab/images/src_6.png';
+
+  // static const String emo_sdk_video = 'assets/emo_sdk/videos/emo_sdk_teaser.mp4';
+  static const String emo_sdk_src1 = 'assets/emosdk/images/src_1.png';
+
+  // static const String exceedium_games_video = 'assets/exceedium_games/videos/teaser.mp4';
+  static const String exceedium_games_src1 =
+      'assets/exceedium_games/images/src_1.png';
+  static const String exceedium_games_src2 =
+      'assets/exceedium_games/images/src_2.png';
+  static const String exceedium_games_src3 =
+      'assets/exceedium_games/images/src_3.png';
+  static const String exceedium_games_src4 =
+      'assets/exceedium_games/images/src_4.png';
+
+  static const String flesh_insane_team_video =
+      'assets/flesh_insane_team/videos/teaser.mp4';
+  static const String flesh_insane_team_src1 =
+      'assets/flesh_insane_team/images/src_1.jpg';
+  static const String flesh_insane_team_src2 =
+      'assets/flesh_insane_team/images/src_2.jpg';
+  static const String flesh_insane_team_src3 =
+      'assets/flesh_insane_team/images/src_3.jpg';
+  static const String flesh_insane_team_src4 =
+      'assets/flesh_insane_team/images/src_4.jpg';
+  static const String flesh_insane_team_src5 =
+      'assets/flesh_insane_team/images/src_5.jpg';
+  static const String flesh_insane_team_src6 =
+      'assets/flesh_insane_team/images/src_6.png';
+  static const String flesh_insane_team_src7 =
+      'assets/flesh_insane_team/images/src_7.jpg';
+  static const String flesh_insane_team_src8 =
+      'assets/flesh_insane_team/images/src_8.jpg';
+  static const String flesh_insane_team_src9 =
+      'assets/flesh_insane_team/images/src_9.png';
+
+  static const String iz_hazard_video = 'assets/iz_hazard/videos/teaser.mp4';
+  static const String iz_hazard_src1 = 'assets/iz_hazard/images/src_1.png';
+  static const String iz_hazard_src2 = 'assets/iz_hazard/images/src_2.png';
+  static const String iz_hazard_src3 = 'assets/iz_hazard/images/src_3.png';
+  static const String iz_hazard_src4 = 'assets/iz_hazard/images/src_4.png';
+  static const String iz_hazard_src5 = 'assets/iz_hazard/images/src_5.png';
+  static const String iz_hazard_src6 = 'assets/iz_hazard/images/src_6.png';
+  static const String iz_hazard_src7 = 'assets/iz_hazard/images/src_7.png';
+  static const String iz_hazard_src8 = 'assets/iz_hazard/images/src_8.png';
+
+  static const String sigil_games_video =
+      'assets/sigil_games/videos/teaser.mp4';
+  static const String sigil_games_src1 = 'assets/sigil_games/images/src_1.jpg';
+  static const String sigil_games_src2 = 'assets/sigil_games/images/src_2.png';
+  static const String sigil_games_src3 = 'assets/sigil_games/images/src_3.jpg';
+  static const String sigil_games_src4 = 'assets/sigil_games/images/src_4.jpg';
+  static const String sigil_games_src5 = 'assets/sigil_games/images/src_5.jpg';
+  static const String sigil_games_src6 = 'assets/sigil_games/images/src_6.jpg';
+  static const String sigil_games_src7 = 'assets/sigil_games/images/src_7.jpg';
+  static const String sigil_games_src8 = 'assets/sigil_games/images/src_8.jpg';
+  static const String sigil_games_src9 = 'assets/sigil_games/images/src_9.png';
+  static const String sigil_games_src10 =
+      'assets/sigil_games/images/src_10.png';
+  static const String sigil_games_src11 =
+      'assets/sigil_games/images/src_11.png';
+  static const String sigil_games_src12 =
+      'assets/sigil_games/images/src_12.png';
+  static const String sigil_games_src13 =
+      'assets/sigil_games/images/src_13.png';
+
+  static const String visual_floss_video =
+      'assets/visual_floss/videos/teaser.mp4';
+  static const String visual_floss_src1 =
+      'assets/visual_floss/images/src_1.jpg';
+  static const String visual_floss_src2 =
+      'assets/visual_floss/images/src_2.jpg';
+  static const String visual_floss_src3 =
+      'assets/visual_floss/images/src_3.jpg';
+  static const String visual_floss_src4 =
+      'assets/visual_floss/images/src_4.jpg';
+  static const String visual_floss_src5 =
+      'assets/visual_floss/images/src_5.jpg';
+}
