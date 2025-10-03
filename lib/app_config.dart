fbe2e988be44d70f1572d3684bf90c82cc943d51
Iskander2323 <iska200314@gmail.com>
2025-10-03 17:09:02 +0500

    Add GameModel and StudioModel getters to AppConfig
    Introduces static getters for various GameModel and StudioModel instances in AppConfig, providing structured access to game and studio data. This refactor centralizes game and studio definitions, making them easier to manage and reference throughout the application.

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -1,5 +1,8 @@
 import 'dart:developer';
 
+import 'package:emosdk_launcher/app_assets.dart';
+import 'package:emosdk_launcher/components/data/game_model.dart';
+import 'package:emosdk_launcher/components/data/studio_model.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppConfig {
@@ -31,6 +34,129 @@ class AppConfig {
     return studios;
   }
 
+  static GameModel get theSacredMemory => GameModel(
+        gameName: 'The Sacred Memory',
+        description: AppAssets.visual_floss_description,
+        gamePath: VISUAL_FLOSS_GAME_PATH,
+        processName: VISUAL_FLOSS_PROCESS,
+        qrAssetPath: AppAssets.visual_floss_qr,
+        videoPath: AppAssets.visual_floss_video,
+        imagesList: [
+          AppAssets.visual_floss_src1,
+          AppAssets.visual_floss_src2,
+          AppAssets.visual_floss_src3,
+          AppAssets.visual_floss_src4,
+          AppAssets.visual_floss_src5,
+        ],
+      );
+
+  static GameModel get reverb => GameModel(
+        gameName: 'Reverb',
+        description: AppAssets.echo_games_lab_description,
+        gamePath: ECHO_GAMES_LAB_GAME_PATH,
+        processName: ECHO_GAMES_LAB_PROCESS,
+        qrAssetPath: AppAssets.echo_games_lab_qr,
+        videoPath: AppAssets.echo_games_lab_video,
+        imagesList: [
+          AppAssets.echo_games_lab_src1,
+          AppAssets.echo_games_lab_src2,
+          AppAssets.echo_games_lab_src3,
+          AppAssets.echo_games_lab_src4,
+          AppAssets.echo_games_lab_src5,
+          AppAssets.echo_games_lab_src6,
+        ],
+      );
+
+  static GameModel get shakeIt => GameModel(
+        gameName: 'Shake it',
+        description: AppAssets.exceedium_games_description,
+        gamePath: EXCEEDIUM_GAMES_GAME_PATH,
+        processName: EXCEEDIUM_GAMES_PROCESS,
+        qrAssetPath: AppAssets.exceedium_games_qr,
+        videoPath: '', //AppAssets.exceedium_games_video,
+        imagesList: [
+          AppAssets.exceedium_games_src1,
+          AppAssets.exceedium_games_src2,
+          AppAssets.exceedium_games_src3,
+          AppAssets.exceedium_games_src4,
+        ],
+      );
+
+  static GameModel get divinum => GameModel(
+        gameName: 'Divinum',
+        description: AppAssets.sigil_games_description,
+        gamePath: SIGIL_GAMES_GAME_PATH,
+        processName: SIGIL_GAMES_PROCESS,
+        qrAssetPath: AppAssets.sigil_games_qr,
+        videoPath: AppAssets.sigil_games_video,
+        imagesList: [
+          AppAssets.sigil_games_src1,
+          AppAssets.sigil_games_src2,
+          AppAssets.sigil_games_src3,
+          AppAssets.sigil_games_src4,
+          AppAssets.sigil_games_src5,
+          AppAssets.sigil_games_src6,
+          AppAssets.sigil_games_src7,
+          AppAssets.sigil_games_src8,
+          AppAssets.sigil_games_src9,
+          AppAssets.sigil_games_src10,
+          AppAssets.sigil_games_src11,
+          AppAssets.sigil_games_src12,
+          AppAssets.sigil_games_src13,
+        ],
+      );
+
+  static GameModel get gostOfTime => GameModel(
+        gameName: 'GOST of Time',
+        description: AppAssets.iz_hazard_description,
+        gamePath: IZ_HAZARD_GAME_PATH,
+        processName: IZ_HAZARD_PROCESS,
+        qrAssetPath: AppAssets.iz_hazard_qr,
+        videoPath: AppAssets.iz_hazard_video,
+        imagesList: [
+          AppAssets.iz_hazard_src1,
+          AppAssets.iz_hazard_src2,
+          AppAssets.iz_hazard_src3,
+          AppAssets.iz_hazard_src4,
+          AppAssets.iz_hazard_src5,
+          AppAssets.iz_hazard_src6,
+          AppAssets.iz_hazard_src7,
+          AppAssets.iz_hazard_src8,
+        ],
+      );
+
+  static GameModel get fleshInsaneTeamGame => GameModel(
+        gameName: 'Fleshinsane Forgotten Empire',
+        description: AppAssets.flesh_insane_team_description,
+        gamePath: FLESH_INSANE_TEAM_GAME_PATH,
+        processName: FLESH_INSANE_TEAM_PROCESS,
+        qrAssetPath: AppAssets.flesh_insane_team_qr,
+        videoPath: AppAssets.flesh_insane_team_video,
+        imagesList: [
+          AppAssets.flesh_insane_team_src1,
+          AppAssets.flesh_insane_team_src2,
+          AppAssets.flesh_insane_team_src3,
+          AppAssets.flesh_insane_team_src4,
+          AppAssets.flesh_insane_team_src5,
+          AppAssets.flesh_insane_team_src6,
+          AppAssets.flesh_insane_team_src7,
+          AppAssets.flesh_insane_team_src8,
+          AppAssets.flesh_insane_team_src9,
+        ],
+      );
+  
+  static GameModel get emoSDKGame => GameModel(
+        gameName: 'EmoSDK',
+        description: '',
+        gamePath: EMOSDK_GAME_PATH,
+        processName: EMOSDK_PROCESS,
+        qrAssetPath: '',
+        videoPath: '', //AppAssets.emosdk_video,
+        imagesList: [
+          AppAssets.emo_sdk_src1,
+        ],
+      );
+
   static String get VISUAL_FLOSS_PROCESS => dotenv.env['VISUAL_FLOSS_PROCESS'] ?? 'CalculatorApp.exe';
   static String get ECHO_GAMES_LAB_PROCESS => dotenv.env['ECHO_GAMES_LAB_PROCESS'] ?? 'CalculatorApp.exe';
   static String get EXCEEDIUM_GAMES_PROCESS => dotenv.env['EXCEEDIUM_GAMES_PROCESS'] ?? 'CalculatorApp.exe';
@@ -46,6 +172,41 @@ class AppConfig {
   static String get IZ_HAZARD_GAME_PATH => dotenv.env['IZ_HAZARD_GAME_PATH'] ?? 'calc.exe';
   static String get FLESH_INSANE_TEAM_GAME_PATH => dotenv.env['FLESH_INSANE_TEAM_GAME_PATH'] ?? 'calc.exe';
   static String get EMOSDK_GAME_PATH => dotenv.env['EMOSDK_GAME_PATH'] ?? 'calc.exe';
+
+  static StudioModel get visualFloss => StudioModel(
+        studioName: 'Visual Floss',
+        games: [theSacredMemory, reverb],
+      );
+
+  static StudioModel get echoGamesLab => StudioModel(
+        studioName: 'Echo Games Lab',
+        games: [reverb],
+      );
+
+  static StudioModel get exceediumGames => StudioModel(
+        studioName: 'Exceedium Games',
+        games: [shakeIt],
+      );
+
+  static StudioModel get sigilGames => StudioModel(
+        studioName: 'Sigil Games',
+        games: [divinum],
+      );
+
+  static StudioModel get izHazard => StudioModel(
+        studioName: 'Iz Hazard',
+        games: [gostOfTime],
+      );
+
+  static StudioModel get fleshInsaneTeam => StudioModel(
+        studioName: 'Flesh Insane Team',
+        games: [fleshInsaneTeamGame],
+      );
+
+  static StudioModel get emoSDK => StudioModel(
+        studioName: 'EmoSDK',
+        games: [emoSDKGame],
+      );
 
   Future<void> load() async {
     try {
